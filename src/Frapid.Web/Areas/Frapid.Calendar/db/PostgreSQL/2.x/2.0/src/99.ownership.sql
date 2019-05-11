309b28a27f7b3bd20b1ef6525f21f19ab1e6834b
bi od <binodnp@outlook.com>
2019-05-11 15:03:00 +0545

    - Updated ownership.sql to fix a bug introduced by a breaking change in PostgreSQL 11. - Rebundled all SQL files.

--- src/Frapid.Web/Areas/Frapid.Calendar/db/PostgreSQL/2.x/2.0/src/99.ownership.sql
+++ src/Frapid.Web/Areas/Frapid.Calendar/db/PostgreSQL/2.x/2.0/src/99.ownership.sql
@@ -39,25 +39,41 @@ LANGUAGE plpgsql;
 DO
 $$
     DECLARE this record;
+    DECLARE _version_number integer = current_setting('server_version_num')::integer;
 BEGIN
     IF(CURRENT_USER = 'frapid_db_user') THEN
         RETURN;
     END IF;
 
-    FOR this IN 
-    SELECT 'ALTER '
-        || CASE WHEN p.proisagg THEN 'AGGREGATE ' ELSE 'FUNCTION ' END
-        || quote_ident(n.nspname) || '.' || quote_ident(p.proname) || '(' 
-        || pg_catalog.pg_get_function_identity_arguments(p.oid) || ') OWNER TO frapid_db_user;' AS sql
-    FROM   pg_catalog.pg_proc p
-    JOIN   pg_catalog.pg_namespace n ON n.oid = p.pronamespace
-    WHERE  NOT n.nspname = ANY(ARRAY['pg_catalog', 'information_schema'])
-    LOOP        
-        EXECUTE this.sql;
-    END LOOP;
+    IF(_version_number < 110000) THEN
+        FOR this IN 
+        SELECT 'ALTER '
+            || CASE WHEN p.proisagg THEN 'AGGREGATE ' ELSE 'FUNCTION ' END
+            || quote_ident(n.nspname) || '.' || quote_ident(p.proname) || '(' 
+            || pg_catalog.pg_get_function_identity_arguments(p.oid) || ') OWNER TO frapid_db_user;' AS sql
+        FROM   pg_catalog.pg_proc p
+        JOIN   pg_catalog.pg_namespace n ON n.oid = p.pronamespace
+        WHERE  NOT n.nspname = ANY(ARRAY['pg_catalog', 'information_schema'])
+        LOOP        
+            EXECUTE this.sql;
+        END LOOP;
+    ELSE
+        FOR this IN 
+        SELECT 'ALTER '
+            || CASE p.prokind WHEN 'a' THEN 'AGGREGATE ' ELSE 'FUNCTION ' END
+            || quote_ident(n.nspname) || '.' || quote_ident(p.proname) || '(' 
+            || pg_catalog.pg_get_function_identity_arguments(p.oid) || ') OWNER TO frapid_db_user;' AS sql
+        FROM   pg_catalog.pg_proc p
+        JOIN   pg_catalog.pg_namespace n ON n.oid = p.pronamespace
+        WHERE  NOT n.nspname = ANY(ARRAY['pg_catalog', 'information_schema'])
+        LOOP        
+            EXECUTE this.sql;
+        END LOOP;
+    END IF;
 END
 $$
 LANGUAGE plpgsql;
+
 
 
 DO
@@ -166,22 +182,37 @@ LANGUAGE plpgsql;
 DO
 $$
     DECLARE this record;
+    DECLARE _version_number integer = current_setting('server_version_num')::integer;
 BEGIN
     IF(CURRENT_USER = 'report_user') THEN
         RETURN;
     END IF;
 
-    FOR this IN 
-    SELECT 'GRANT EXECUTE ON '
-        || CASE WHEN p.proisagg THEN 'AGGREGATE ' ELSE 'FUNCTION ' END
-        || quote_ident(n.nspname) || '.' || quote_ident(p.proname) || '(' 
-        || pg_catalog.pg_get_function_identity_arguments(p.oid) || ') TO report_user;' AS sql
-    FROM   pg_catalog.pg_proc p
-    JOIN   pg_catalog.pg_namespace n ON n.oid = p.pronamespace
-    WHERE  NOT n.nspname = ANY(ARRAY['pg_catalog', 'information_schema'])
-    LOOP        
-        EXECUTE this.sql;
-    END LOOP;
+    IF(_version_number < 110000) THEN
+        FOR this IN 
+        SELECT 'GRANT EXECUTE ON '
+            || CASE WHEN p.proisagg THEN 'AGGREGATE ' ELSE 'FUNCTION ' END
+            || quote_ident(n.nspname) || '.' || quote_ident(p.proname) || '(' 
+            || pg_catalog.pg_get_function_identity_arguments(p.oid) || ') TO report_user;' AS sql
+        FROM   pg_catalog.pg_proc p
+        JOIN   pg_catalog.pg_namespace n ON n.oid = p.pronamespace
+        WHERE  NOT n.nspname = ANY(ARRAY['pg_catalog', 'information_schema'])
+        LOOP        
+            EXECUTE this.sql;
+        END LOOP;
+    ELSE
+        FOR this IN 
+        SELECT 'GRANT EXECUTE ON '
+            || CASE p.prokind WHEN 'a' THEN 'AGGREGATE ' ELSE 'FUNCTION ' END
+            || quote_ident(n.nspname) || '.' || quote_ident(p.proname) || '(' 
+            || pg_catalog.pg_get_function_identity_arguments(p.oid) || ') TO report_user;' AS sql
+        FROM   pg_catalog.pg_proc p
+        JOIN   pg_catalog.pg_namespace n ON n.oid = p.pronamespace
+        WHERE  NOT n.nspname = ANY(ARRAY['pg_catalog', 'information_schema'])
+        LOOP        
+            EXECUTE this.sql;
+        END LOOP;
+    END IF;
 END
 $$
 LANGUAGE plpgsql;
