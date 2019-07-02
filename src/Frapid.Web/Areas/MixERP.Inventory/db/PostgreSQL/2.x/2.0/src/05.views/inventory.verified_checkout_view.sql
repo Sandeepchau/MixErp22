fdb6737597b5baa3ec6b56f039decf963bb6bcbb
Sandeep Chaudhary <35701923+Sandeepchau@users.noreply.github.com>
2019-07-02 21:04:40 +0530

    Add files via upload

---
+++ src/Frapid.Web/Areas/MixERP.Inventory/db/PostgreSQL/2.x/2.0/src/05.views/inventory.verified_checkout_view.sql
@@ -0,0 +1,12 @@
+﻿DROP MATERIALIZED VIEW IF EXISTS inventory.verified_checkout_view;
+
+CREATE MATERIALIZED VIEW inventory.verified_checkout_view
+AS
+SELECT * FROM inventory.checkout_view
+WHERE verification_status_id > 0;
+
+ALTER MATERIALIZED VIEW inventory.verified_checkout_view
+OWNER TO frapid_db_user;
+
+CREATE UNIQUE INDEX verified_checkout_view_checkout_detail_id_uix
+ON inventory.verified_checkout_view(checkout_detail_id);
