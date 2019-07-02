d23711384bfd32aebd627411a16de392691c9a62
Sandeep Chaudhary <35701923+Sandeepchau@users.noreply.github.com>
2019-07-02 20:59:19 +0530

    Add files via upload

---
+++ src/Frapid.Web/Areas/MixERP.Inventory/db/PostgreSQL/2.1.update/src/03.menus/menus.sql
@@ -0,0 +1,12 @@
+﻿SELECT * FROM core.create_menu('MixERP.Inventory', 'InventorySetup', 'Inventory Setup', '/dashboard/inventory/setup/is', 'content', 'Setup');
+SELECT * FROM core.create_menu('MixERP.Inventory', 'TransactionDetails', 'Transaction Details', '/dashboard/reports/view/Areas/MixERP.Inventory/Reports/TransactionDetails.xml', 'content', 'Setup');
+
+
+SELECT * FROM auth.create_app_menu_policy
+(
+    'Admin', 
+    core.get_office_id_by_office_name('Default'), 
+    'MixERP.Inventory',
+    '{*}'::text[]
+);
+
